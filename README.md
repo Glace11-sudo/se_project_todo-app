cc27d3773dfce3a2a6b1ffcd807e02613cd2c1a6
Algie Lovelace <lovelacealgie11@gmail.com>
2025-07-09 18:11:57 -0400

    se_project_todo_app

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # Simple Todo App
 
-Give a brief description of the project here. Feel free to give it a different name.
+The name of this project is the ToDo-app project. it is a basic Todo app format for keeping one on task and organized.
 
 ## Functionality
 
-Give a more detailed explanation of the project and its functionality.
+In this app you can add a todo task by clicking the todo button. Once clicked, it will pull up a form with two elements. The first element is what needs to be done. The second element provides a date for when the task needs to be completed. Once both feilds are filled with the proper text, click the create button and the task will be posted to the app.
 
 ## Technology
 
-Give a description of the technologies and techniques used. Pictures, GIFs, or screenshots that detail the project features are recommended.
+To accomplish this, the functionality methods are stored inside the FormValidator.js file which is in the components folder. the features in this app is as follows
+
+- Add new todos
+- Mark todos as complete/incomplete
+- Delete individual todos
+- (Optional) Persistent storage with localStorage
 
 ## Deployment
 
 This project is deployed on GitHub Pages:
 
-- ADD LINK HERE
+http://127.0.0.1:5500/index.html
---
+++ components/FormValidator.js
@@ -0,0 +1,89 @@
+class FormValidator {
+  constructor(settings, formEl) {
+    this._inputSelector = settings.inputSelector;
+    this._submitButtonSelector = settings.submitButtonSelector;
+    this._errorClass = settings.errorClass;
+    this._inputErrorClass = settings.inputErrorClass;
+    this._inactiveButtonClass = settings.inactiveButtonClass;
+    this._formEl = formEl;
+  }
+
+  _showInputError(inputElement) {
+    const errorElement = this._formEl.querySelector(
+      `#${inputElement.id}-error`
+    );
+    inputElement.classList.add(this._inputErrorClass);
+    errorElement.classList.add(this._errorClass);
+    errorElement.textContent = inputElement.validationMessage;
+  }
+
+  _hideInputError(inputElement) {
+    const errorElement = this._formEl.querySelector(
+      `#${inputElement.id}-error`
+    );
+    inputElement.classList.remove(this._inputErrorClass);
+    errorElement.classList.remove(this._errorClass);
+    errorElement.textContent = "";
+  }
+
+  _hasInvalidInput(inputList) {
+    return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvalidInput(this._inputList)) {
+      this._buttonElement.classList.add(this._inactiveButtonClass);
+      this._buttonElement.disabled = true;
+    } else {
+      this._buttonElement.classList.remove(this._inactiveButtonClass);
+      this._buttonElement.disabled = false;
+    }
+  }
+
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  }
+
+  _setEventListeners() {
+    this._inputList = Array.from(
+      this._formEl.querySelectorAll(this._inputSelector)
+    );
+    this._buttonElement = this._formEl.querySelector(
+      this._submitButtonSelector
+    );
+
+    this._toggleButtonState(this._inputList, this._buttonElement);
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState(this._inputList, this._buttonElement);
+      });
+    });
+  }
+
+  enableValidation() {
+    this._formEl.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+
+    this._setEventListeners();
+  }
+
+  resetValidation() {
+    this._inputList.forEach((input) => {
+      input.value = "";
+      this._hideInputError(input);
+    });
+    this._submitButtonSelector.disabled = true;
+    this._submitButtonSelector.classList.add("button_disabled");
+  }
+}
+
+export default FormValidator;
